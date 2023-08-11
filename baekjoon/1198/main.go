c13d11663b3ef20a1a53c556f8c06d8df2f259f1
jeonghyeonee <jlh1218@naver.com>
2023-08-11 15:56:39 +0900

    solving...

---
+++ baekjoon/1198/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	// Inpute the number of N (3 <= N <= 35)
+	fmt.Scanln(&n)
+
+	// fmt.Scanln()
+
+	// reader := bufio.NewReader(os.Stdin)
+	
+	for i := 0; i < n; i++ {
+		fmt.Scanln("%d%d", )
+	}
+
+	fmt.Println()
+}
